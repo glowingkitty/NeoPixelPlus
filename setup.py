e576a3bc0a5930ab8c16407e1923724c81e53ac8
marcoEDU <contact@marcobartsch.me>
2020-10-16 11:54:20 +0200

    update pip package

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neopixel_plus",  # Replace with your own username
-    version="0.3.8",
+    version="1.0.0",
     author="Marco",
     author_email=None,
-    description="The NeoPixel library plus some extras, for example a testing mode - so you can see how your LEDs would behave directly in the terminal, without any extra hardware..",
+    description="The NeoPixel library plus animations and terminal testing mode - so you can see how your LEDs would behave directly in the terminal, without any microcontroller.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/glowingkitty/NeoPixelPlus",

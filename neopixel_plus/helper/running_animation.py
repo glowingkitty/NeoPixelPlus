7b8370f28e7d8af9363aa0e957caf633ed385a01
marcoEDU <contact@marcobartsch.me>
2020-11-15 19:47:59 +0100

    fixed animation not starting when no previous animation was running

--- neopixel_plus/helper/running_animation.py
+++ neopixel_plus/helper/running_animation.py
@@ -13,12 +13,15 @@ class RunningAnimation:
         f = open(os.path.join(dirname, 'animation_running.txt'), 'w')
         f.write('')
         f.close()
+        print('Created {}'.format(os.path.join(dirname, 'animation_running.txt')))
 
     def check_animation_running():
         if not os.path.exists(os.path.join(dirname, 'animation_running.txt')):
             f = open(os.path.join(dirname, 'animation_stopped.txt'), 'w')
             f.write('')
             f.close()
+            print('Created {}'.format(os.path.join(
+                dirname, 'animation_stopped.txt')))
             return False
         return True
 
@@ -26,10 +29,20 @@ class RunningAnimation:
         # check if "animation_running.txt" exists & delete it
         if os.path.exists(os.path.join(dirname, 'animation_running.txt')):
             os.remove(os.path.join(dirname, 'animation_running.txt'))
+            print('Removed {}'.format(os.path.join(
+                dirname, 'animation_running.txt')))
 
             # while "animation_stopped.txt" doesn't exists → wait
+            tried_find_animation_stopped = 0
             while not os.path.exists(os.path.join(dirname, 'animation_stopped.txt')):
+                if tried_find_animation_stopped > 10:
+                    print(
+                        'Seems no previous animation is running. Skipped removing animation_stopped.txt')
+                    return
+                tried_find_animation_stopped += 1
                 time.sleep(0.1)
 
             # delete "animation_stopped.txt" and start new animation
             os.remove(os.path.join(dirname, 'animation_stopped.txt'))
+            print('Removed {}'.format(os.path.join(
+                dirname, 'animation_stopped.txt')))
